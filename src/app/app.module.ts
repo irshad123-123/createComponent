74409e70b16c489843f151421f954dd4f3522873
irshad123-123 <shaikhirshadwahab@gmail.com>
2025-11-04 21:26:19 +0530

    fix the spelling of component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { UsersComponent } from './users/users.component';
+import { StudentsComponent } from './students/students.component';
+import { TodosComponent } from './todos/todos.component';
+import { MoviesComponent } from './movies/movies.component';
+import { LaptopComponent } from './laptop/laptop.component';
+import { MobileComponent } from './mobile/mobile.component';
+import { DeviceComponent } from './device/device.component';
+import { ImagesComponent } from './images/images.component';
+import { BikesComponent } from './bikes/bikes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    UsersComponent,
+    StudentsComponent,
+    TodosComponent,
+    MoviesComponent,
+    LaptopComponent,
+    MobileComponent,
+    DeviceComponent,
+    ImagesComponent,
+    BikesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/device/device.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector : 'app-device',
+    templateUrl : './device.component.html',
+    styleUrls : ['./device.component.scss']
+})
+export class DeviceComponent{
+
+}
